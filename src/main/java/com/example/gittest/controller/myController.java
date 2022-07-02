4e042f2cfce1e441de4ca6a0dc35f129cbee1517
hliangjin <1074315791@qq.com>
2022-07-03 01:03:48 +0800

    新增分支

--- src/main/java/com/example/gittest/controller/myController.java
+++ src/main/java/com/example/gittest/controller/myController.java
@@ -1,5 +1,6 @@
 package com.example.gittest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,13 @@ public class myController {
         System.out.println("第二次修改");
         System.out.println("第三次修改");
         System.out.println("第四次修改");
+
+        return  "ok";
+    }
+
+    @GetMapping("/user")
+    public  String  getUser(){
+        System.out.println("新建DEC分支");
 
         return  "ok";
     }
